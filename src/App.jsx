a701a2b883849c17b7af08397658a41506a12f97
Aditya Kumar Shaw <aks09adi@gmail.com>
2026-07-29 12:40:53 +0530

    Add Loader component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import About from './components/About'
@@ -6,10 +7,12 @@ import Projects from './components/Projects'
 import DSA from './components/DSA'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
+import Loader from './components/Loader'
 
 export default function App() {
   return (
     <div className="bg-[#080808] text-slate-100 font-sans antialiased overflow-x-hidden">
+      <Loader />
       <Navbar />
       <Hero />
       <About />
