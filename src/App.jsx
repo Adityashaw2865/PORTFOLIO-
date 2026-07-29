061a23b9c8943036fcacae993fa9a96509403179
Aditya Kumar Shaw <aks09adi@gmail.com>
2026-07-29 12:55:57 +0530

    Add CommandPalette component to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import About from './components/About'
@@ -8,11 +7,13 @@ import DSA from './components/DSA'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
 import Loader from './components/Loader'
+import CommandPalette from './components/CommandPalette'
 
 export default function App() {
   return (
     <div className="bg-[#080808] text-slate-100 font-sans antialiased overflow-x-hidden">
       <Loader />
+      <CommandPalette />
       <Navbar />
       <Hero />
       <About />
